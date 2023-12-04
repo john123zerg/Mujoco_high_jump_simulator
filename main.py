475274777c09ca5fa418209a6289efaabdca9fba
john123zerg <63462803+john123zerg@users.noreply.github.com>
2023-12-03 20:29:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import os
 import argparse
 from utils import parse_pattern,train,test,enable_xml_wall,override_env
-
+import time
 
 # Create directories to hold models and logs
 model_dir = "models"
@@ -23,13 +23,17 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--train', action='store_true')
     parser.add_argument('-s', '--test', metavar='path_to_model')
     parser.add_argument('-p','--policy',default='MlpPolicy')
+    parser.add_argument('-cs','--critic_size',default='32')
     parser.add_argument('-w','--wall',default=0)
     parser.add_argument('-ws','--wall_size',default=None)
     parser.add_argument('-tw','--test_wall',default=0)
     parser.add_argument('-z','--reward_function',default=0)
-
+    parser.add_argument('-tws','--test_wall_size',default=None)
     args = parser.parse_args()
-    enable_xml_wall.modify_xml_file(args.gymenv.lower(),args.wall,args.wall_size)
+    if args.test_wall!=0 or args.wall!=0:
+        enable_xml_wall.modify_xml_file(args.gymenv.lower(),args.wall,args.test_wall_size)
+    else:
+        enable_xml_wall.modify_xml_file(args.gymenv.lower(),args.wall,args.wall_size)
     print(f'reward_{args.reward_function}')
     if args.reward_function=='1':
         print('modifying environment')
@@ -39,7 +43,7 @@ if __name__ == '__main__':
         override_env.delete_reward(args.gymenv,args.wall,args.wall_size)
     if args.train:
         gymenv = gym.make(args.gymenv+'-v4', render_mode='None')
-        train.train_model(gymenv, args.sb3_algo,args.policy,args.wall,args.wall_size,args.reward_function)
+        train.train_model(gymenv, args.sb3_algo,args.policy,args.critic_size,args.wall,args.wall_size,args.reward_function)
 
     if args.test:
         test_file=parse_pattern.search_file(args.gymenv+'-'+args.sb3_algo,args.policy,args.wall,args.wall_size,args.test_wall,args.reward_function)
