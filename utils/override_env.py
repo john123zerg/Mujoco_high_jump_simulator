96a7c99bd428902459a78a9ef9ca4d07287260b5
john123zerg <63462803+john123zerg@users.noreply.github.com>
2023-12-05 18:49:03 -0600

    Update override_env.py

--- utils/override_env.py
+++ utils/override_env.py
@@ -5,19 +5,19 @@ from gymnasium.envs import mujoco
 def reward_function(env):
     match env:
         case 'half_cheetah':
-            list_=[1,0]
+            list_=[0,1]
             height_=0.7
         case 'walker2d':
-            list_=[1,9,10,0]
+            list_=[0,1,9,10]
             height_=1.25
         case 'humanoid':
-            list_=[2,24,26,0]
+            list_=[0,2,24,26]
             height_=1.4
         case 'ant':
-            list_=[2,15,17,0]
+            list_=[0,2,15,17]
             height_=0.75
         case 'hopper':
-            list_=[1,6,7,0]
+            list_=[0,1,6,7]
             height_=1.25
 
     function_code=function_reward_list(list_,height_)
@@ -27,25 +27,25 @@ def reward_function(env):
 def function_reward_list(list_,height_):
     if len(list_)==2:
              
-            function_code= [f"        height_reward =  100*(self.state_vector()[{list_[0]}]/{height_}) if self.state_vector()[{list_[0]}]/{height_} > 1.0 else 0.0 \n",
-    f"        height_reward_2 = 10*(self.state_vector()[{list_[0]}]/{height_}) if self.state_vector()[{list_[0]}]/{height_} > 0.5 and self.state_vector()[{list_[0]}]/{height_} < 1.0 else 0.0\n",
-    f"        height_reward_3 = -10*abs((self.state_vector()[{list_[0]}]/{height_})) if self.state_vector()[{list_[0]}]/{height_} < 0.5 else 0.0\n",                 
-    f"        x_reward = 100 * self.state_vector()[{list_[-1]}] if self.state_vector()[{list_[-1]}] > 1.0 else 0.0\n",                       
-    f"        x_reward_2 = 10 * self.state_vector()[{list_[-1]}] if self.state_vector()[{list_[-1]}] > 0.5 and self.state_vector()[{list_[-1]}] < 1.0 else 0.0\n",
-    f"        x_reward_3 = -10 * self.state_vector()[{list_[-1]}] if self.state_vector()[{list_[-1]}] < 0.5 else 0.0\n",
+            function_code= [f"        height_reward =  100*(self.state_vector()[{list_[0]}]/{height_}) if self.state_vector()[{list_[1]}]/{height_} > 1.0 else 0.0 \n",
+    f"        height_reward_2 = 10*(self.state_vector()[{list_[1]}]/{height_}) if self.state_vector()[{list_[1]}]/{height_} > 0.5 and self.state_vector()[{list_[1]}]/{height_} < 1.0 else 0.0\n",
+    f"        height_reward_3 = -10*abs((self.state_vector()[{list_[1]}]/{height_})) if self.state_vector()[{list_[1]}]/{height_} < 0.5 else 0.0\n",                 
+    f"        x_reward = 100 * self.state_vector()[{list_[0]}] if self.state_vector()[{list_[0]}] > 1.0 else 0.0\n",                       
+    f"        x_reward_2 = 10 * self.state_vector()[{list_[0]}] if self.state_vector()[{list_[0]}] > 0.5 and self.state_vector()[{list_[0]}] < 1.0 else 0.0\n",
+    f"        x_reward_3 = -10 * self.state_vector()[{list_[0]}] if self.state_vector()[{list_[0]}] < 0.5 else 0.0\n",
     f"        total_reward=height_reward + height_reward_2 + height_reward_3 + x_reward + x_reward_2 + x_reward_3\n",
     "        reward = forward_reward  + total_reward - ctrl_cost\n"]
     elif len(list_)==4:
-            function_code= [f"        height_reward =  100*(self.state_vector()[{list_[0]}]/{height_}) if self.state_vector()[{list_[0]}]/{height_} > 1.0 else 0.0 \n",
-    f"        height_reward_2 = 10*(self.state_vector()[{list_[0]}]/{height_}) if self.state_vector()[{list_[0]}]/{height_} > 0.5 and self.state_vector()[{list_[0]}]/{height_} < 1.0 else 0.0\n",
-    f"        height_reward_3 = -10*abs((self.state_vector()[{list_[0]}]/{height_})) if self.state_vector()[{list_[0]}]/{height_} < 0.5 else 0.0\n",                       
-    f"        x_reward = 100 * self.state_vector()[{list_[-1]}] if self.state_vector()[{list_[-1]}] > 3.0 else 0.0\n",                       
-    f"        x_reward_2 = 10 * self.state_vector()[{list_[-1]}] if self.state_vector()[{list_[-1]}] > 0.5 and self.state_vector()[{list_[3]}] < 3.0 else 0.0\n",
-    f"        x_reward_3 = -10 * abs(self.state_vector()[{list_[-1]}]) if self.state_vector()[{list_[-1]}] < 0.5 else 0.0\n",
-    f"        x_velocity_reward = 10*self.state_vector()[{list_[1]}] if self.state_vector()[{list_[1]}] > 1.0 else  0.0\n",  
-    f"        x_velocity_reward_2 = 5*self.state_vector()[{list_[1]}] if self.state_vector()[{list_[1]}] < 1.0 and self.state_vector()[{list_[1]}] > 0.5 else 0.0\n",
-    f"        z_velocity_reward = 10*self.state_vector()[{list_[2]}] if self.state_vector()[{list_[2]}] > 1.0 else 0.0\n", 
-    f"        z_velocity_reward_2 = 5*self.state_vector()[{list_[2]}] if self.state_vector()[{list_[2]}] < 1.0 and self.state_vector()[{list_[2]}] > 0.5 else 0.0\n",
+            function_code= [f"        height_reward =  100*(self.state_vector()[{list_[1]}]/{height_}) if self.state_vector()[{list_[1]}]/{height_} > 1.0 else 0.0 \n",
+    f"        height_reward_2 = 10*(self.state_vector()[{list_[1]}]/{height_}) if self.state_vector()[{list_[1]}]/{height_} > 0.5 and self.state_vector()[{list_[1]}]/{height_} < 1.0 else 0.0\n",
+    f"        height_reward_3 = -10*abs((self.state_vector()[{list_[1]}]/{height_})) if self.state_vector()[{list_[1]}]/{height_} < 0.5 else 0.0\n",                       
+    f"        x_reward = 100 * self.state_vector()[{list_[0]}] if self.state_vector()[{list_[0]}] > 3.0 else 0.0\n",                       
+    f"        x_reward_2 = 10 * self.state_vector()[{list_[0]}] if self.state_vector()[{list_[0]}] > 0.5 and self.state_vector()[{list_[3]}] < 3.0 else 0.0\n",
+    f"        x_reward_3 = -10 * abs(self.state_vector()[{list_[0]}]) if self.state_vector()[{list_[0]}] < 0.5 else 0.0\n",
+    f"        x_velocity_reward = 10*self.state_vector()[{list_[2]}] if self.state_vector()[{list_[2]}] > 1.0 else  0.0\n",  
+    f"        x_velocity_reward_2 = 5*self.state_vector()[{list_[2]}] if self.state_vector()[{list_[2]}] < 1.0 and self.state_vector()[{list_[2]}] > 0.5 else 0.0\n",
+    f"        z_velocity_reward = 10*self.state_vector()[{list_[3]}] if self.state_vector()[{list_[3]}] > 1.0 else 0.0\n", 
+    f"        z_velocity_reward_2 = 5*self.state_vector()[{list_[3]}] if self.state_vector()[{list_[3]}] < 1.0 and self.state_vector()[{list_[3]}] > 0.5 else 0.0\n",
     f"        total_reward=height_reward + height_reward_2 + height_reward_3 + x_reward + x_reward_2 + x_reward_3 + x_velocity_reward + x_velocity_reward_2 + z_velocity_reward + z_velocity_reward_2\n",
     "        rewards = forward_reward  + total_reward + healthy_reward\n"]
     return function_code
